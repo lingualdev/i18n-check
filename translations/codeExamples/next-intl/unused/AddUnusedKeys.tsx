8897b57c8f416b8f73ae6db6b76d52e0cbf2f789
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-09-30 18:23:51 -0300

    Fix typo in comment for unused keys check

--- translations/codeExamples/next-intl/unused/AddUnusedKeys.tsx
+++ translations/codeExamples/next-intl/unused/AddUnusedKeys.tsx
@@ -2,7 +2,7 @@
 import { useTranslations } from 'next-intl';
 import NavigationLink from './NavigationLink';
 
-// We this for checking the CLI doesn't fail when there are no unused keys
+// We use this for checking the CLI doesn't fail when there are no unused keys
 export default function AddUnusedKeys() {
   const t = useTranslations();
 
